e5902424dd5e0ae057747d56ed2e1b3a275c55c4
Adebayo Salami <salamibolarinwa16@gmail.com>
2021-03-01 00:29:45 +0100

    Am Done

--- HaripayTechnicalAssessmentTest/DiscountServiceTest.cs
+++ HaripayTechnicalAssessmentTest/DiscountServiceTest.cs
@@ -119,5 +119,92 @@ namespace HaripayTechnicalAssessmentTest
             Assert.IsType<Discount>(discountService.GetDiscountByType("Type 1"));
         }
 
+        [Fact]
+        public void GetTotalInvoiceAmount_Invalid_Bill_Amount_Passed()
+        {
+            var options = new DbContextOptionsBuilder<HaripayDataContext>()
+                .UseInMemoryDatabase(databaseName: "Haripay")
+                .Options;
+
+            var context = new HaripayDataContext(options);
+            var discountService = new DiscountService(context);
+            var customerService = new CustomerService(context);
+
+            customerService.CreateCustomer("Test 1", 1);
+            customerService.CreateCustomer("Test 2", 2);
+
+            Assert.Throws<InvalidOperationException>(() => discountService.GetTotalInvoiceAmount(1, 0, false));
+        }
+
+        [Fact]
+        public void GetTotalInvoiceAmount_Invalid_Customer_ID_Passed()
+        {
+            var options = new DbContextOptionsBuilder<HaripayDataContext>()
+                .UseInMemoryDatabase(databaseName: "Haripay")
+                .Options;
+
+            var context = new HaripayDataContext(options);
+            var discountService = new DiscountService(context);
+            var customerService = new CustomerService(context);
+
+            customerService.CreateCustomer("Test 1", 1);
+            customerService.CreateCustomer("Test 2", 2);
+
+            Assert.Throws<InvalidOperationException>(() => discountService.GetTotalInvoiceAmount(0, 200, false));
+        }
+
+        [Fact]
+        public void GetTotalInvoiceAmount_Customer_Not_Exists()
+        {
+            var options = new DbContextOptionsBuilder<HaripayDataContext>()
+                .UseInMemoryDatabase(databaseName: "Haripay")
+                .Options;
+
+            var context = new HaripayDataContext(options);
+            var discountService = new DiscountService(context);
+            var customerService = new CustomerService(context);
+
+            customerService.CreateCustomer("Test 1", 1);
+            customerService.CreateCustomer("Test 2", 2);
+
+            Assert.Throws<InvalidOperationException>(() => discountService.GetTotalInvoiceAmount(35, 200, false));
+        }
+
+        [Fact]
+        public void GetTotalInvoiceAmount_Discount_Not_Exists()
+        {
+            var options = new DbContextOptionsBuilder<HaripayDataContext>()
+                .UseInMemoryDatabase(databaseName: "Haripay")
+                .Options;
+
+            var context = new HaripayDataContext(options);
+            var discountService = new DiscountService(context);
+            var customerService = new CustomerService(context);
+
+            customerService.CreateCustomer("Test 1", 1);
+            customerService.CreateCustomer("Test 2", 2);
+
+            Assert.Throws<InvalidOperationException>(() => discountService.GetTotalInvoiceAmount(10, 200, false, 10));
+        }
+
+        [Fact]
+        public void GetTotalInvoiceAmount_Discount_Is_Inactive()
+        {
+            var options = new DbContextOptionsBuilder<HaripayDataContext>()
+                .UseInMemoryDatabase(databaseName: "Haripay")
+                .Options;
+
+            var context = new HaripayDataContext(options);
+            var discountService = new DiscountService(context);
+            var customerService = new CustomerService(context);
+
+            customerService.CreateCustomer("Test 1", 1);
+            customerService.CreateCustomer("Test 2", 2);
+            discountService.CreateDiscount("Type 1", 20, 1, 2);
+            discountService.CreateDiscount("Type 2", 20, 1, 2);
+
+            Assert.Throws<InvalidOperationException>(() => discountService.GetTotalInvoiceAmount(1, 200, false, 1));
+        }
+
     }
 }
